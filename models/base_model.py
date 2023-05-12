5d623a1c54b55e3e6b043a42d3d634010aadfed8
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-05-12 14:32:03 +0000

    Updated the base_model.py file (*args and **kwargs added in __init__ method)

--- models/base_model.py
+++ models/base_model.py
@@ -5,11 +5,22 @@ from datetime import datetime
 
 class BaseModel:
     """This class defines a base mode"""
-    def __init__(self):
-        """initialization method"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """initialization method
+        Arguments:
+            *args: Non-keyworded variable argument list (not used here)
+            **kwargs: Handles named arguments passed
+        """
+        if kwargs and kwargs != {}:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ["created_at", "updated_at"]:
+                        value = datetime.fromisoformat(value)
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """this method prints id, created_at, updated_at public attributes"""
